8a7d041e6faa03efc8e4330f0bb5882de4f7b2df
Bara <bkonateh1@gmail.com>
2024-03-27 21:13:25 +0100

    add signals

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import AppWrapper from "./components/AppWrapper";
+import { useSignals } from "@preact/signals-react/runtime";
+import { counter } from "./signals";
 
 function App() {
+	useSignals();
 	return (
 		<AppWrapper>
-			<div className="grow bg-black rounded-lg"></div>
+			<div className="grow bg-black rounded-lg">
+				<button
+					className="p-2 bg-blue-500 text-white rounded-lg"
+					onClick={() => counter.value++}
+				>
+					{counter.value}
+				</button>
+			</div>
 		</AppWrapper>
 	);
 }
