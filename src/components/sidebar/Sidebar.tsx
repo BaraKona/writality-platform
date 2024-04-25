f2698181de1f0e65e28d91c1b00ea8096ce156e5
Bara <bkonateh1@gmail.com>
2024-04-25 19:13:31 +0200

    add opening and closing animations

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,6 +6,7 @@ import { useParams } from "@tanstack/react-router";
 import { FolderListItem } from "./FolderListItem";
 import { ScriptListItem } from "./ScriptListItem";
 import { MenuItems } from "./MenuItems";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const Sidebar = () => {
 	useSignals();
@@ -16,50 +17,68 @@ export const Sidebar = () => {
 	const { name } = useParams("file");
 
 	return (
-		<aside
-			className={`p-2 rounded-l-md relative overflow-y-auto bg-backgroundHover ${isSidebarOpen.value ? "w-64 " : "w-0 hidden"}`}
-		>
-			<section className="flex h-full grow">
-				<div
-					className={`w-full overflow-y-auto grow flex flex-col ${isSidebarOpen.value ? "block" : "hidden"}`}
+		<AnimatePresence initial={false} mode="wait" onExitComplete={() => null}>
+			<motion.aside
+				className="rounded-l-md relative overflow-y-auto bg-backgroundHover"
+				animate={isSidebarOpen.value ? "open" : "closed"}
+				initial={false}
+				variants={{
+					open: { opacity: 1, width: 265, display: "block", padding: 8 },
+					closed: { opacity: 1, width: 0, display: "none", padding: 0 },
+				}}
+				exit={{ opacity: 0, display: "hidden" }}
+			>
+				<motion.section
+					className="h-full grow"
+					animate={isSidebarOpen.value ? "showing" : "hidden"}
+					variants={{
+						showing: { opacity: 1, display: "flex" },
+						hidden: {
+							opacity: 0,
+							display: "none",
+							transition: { duration: 0 },
+						},
+					}}
 				>
-					<MenuItems />
-					<ItemSectionWrapper>
-						<div className="flex flex-col gap-0.5 mt-2 flex-grow overflow-y-auto">
-							{data?.map((file, index) => (
-								<>
-									{file.extension === "folder" && (
-										<FolderListItem
-											files={file.children}
-											key={index}
-											name={file.filename}
-											level={0}
-											id={file.filename + index + 0}
-											currentPath={name}
-										/>
-									)}
-									{file.extension === "json" && (
-										<ScriptListItem
-											currentPath={name}
-											key={index}
-											name={file.filename}
-											path={file.path}
-											level={0}
-										/>
-									)}
-								</>
-							))}
+					<div className={`w-full overflow-y-auto grow flex flex-col `}>
+						<MenuItems />
+						<ItemSectionWrapper>
+							<div className="flex flex-col gap-0.5 mt-2 flex-grow overflow-y-auto">
+								{data?.map((file, index) => (
+									<>
+										{file.extension === "folder" && (
+											<FolderListItem
+												files={file.children}
+												key={index}
+												name={file.filename}
+												level={0}
+												id={file.filename + index + 0}
+												currentPath={name}
+											/>
+										)}
+										{file.extension === "json" && (
+											<ScriptListItem
+												currentPath={name}
+												key={index}
+												name={file.filename}
+												path={file.path}
+												level={0}
+											/>
+										)}
+									</>
+								))}
 
-							{data?.length === 0 && (
-								<p className="text-xs text-center text-text">
-									Fresh starts are always good. Create a new file to get
-									started.
-								</p>
-							)}
-						</div>
-					</ItemSectionWrapper>
-				</div>
-			</section>
-		</aside>
+								{data?.length === 0 && (
+									<p className="text-xs text-center text-text">
+										Fresh starts are always good. Create a new file to get
+										started.
+									</p>
+								)}
+							</div>
+						</ItemSectionWrapper>
+					</div>
+				</motion.section>
+			</motion.aside>
+		</AnimatePresence>
 	);
 };
